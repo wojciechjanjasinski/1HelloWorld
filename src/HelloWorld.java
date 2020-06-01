0ae2e3ecdcae157391093e2d255e71df30c0eb51
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-01 22:12:30 +0200

    Create: HelloWorld, UczeSieProgramowania and .gitignore.

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("haha");
+        System.out.print("Hello World! Witaj Świecie!");
+        System.out.println(1_000_000_000_000_000_000L);
+    }
+}
---
+++ src/UczeSieProgramowania.java
@@ -0,0 +1,7 @@
+class UczeSieProgramowania {
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.print("programowania");
+    }
+}
