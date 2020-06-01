e476b05416917d393cb33470be5c5c092306d467
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-01 22:23:15 +0200

    In Line 4 change from "Print" to "Println"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("haha");
-        System.out.print("Hello World! Witaj Świecie!");
+        System.out.println("Hello World! Witaj Świecie!");
         System.out.println(1_000_000_000_000_000_000L);
     }
 }
