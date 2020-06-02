e91d9d94bab73fdfecb63104a2e7d1b55101f8e2
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-02 19:29:45 +0200

    Create DataTypes1 with excersise form online course JavaStart.

---
+++ src/DataTypes1.java
@@ -0,0 +1,8 @@
+class DataTypes1 {
+    public static void main(String[] args) {
+        System.out.println("Cześć Jan, słyszałem, że masz 25 lat i uczysz się programowania. Czy to prawda?");
+        System.out.println(true);
+        System.out.println("Czy Twoje inicjały to J K?");
+        System.out.print("Tak, "+true);
+    }
+}
