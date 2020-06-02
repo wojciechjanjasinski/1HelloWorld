e1fb5081105d276b5ff4d3727f5f8b47fdfffa11
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-02 19:35:18 +0200

    Small changes done in the code.

--- src/DataTypes1.java
+++ src/DataTypes1.java
@@ -2,7 +2,11 @@ class DataTypes1 {
     public static void main(String[] args) {
         System.out.println("Cześć Jan, słyszałem, że masz 25 lat i uczysz się programowania. Czy to prawda?");
         System.out.println(true);
-        System.out.println("Czy Twoje inicjały to J K?");
+        System.out.print("Czy Twoje inicjały to ");
+        System.out.print('J');
+        System.out.print("");
+        System.out.print('K');
+        System.out.println('?');
         System.out.print("Tak, "+true);
     }
 }
