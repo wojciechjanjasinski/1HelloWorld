ce02868f0a649c1930e4a2836b9de396c8b879d3
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-02 19:43:23 +0200

    Create the correct file BasicTypes1

---
+++ src/BasicTypes1.java
@@ -0,0 +1,9 @@
+class BasicTypes1 {
+    public static void main(String[] args) {
+        System.out.println("Mam Audi A4");
+        System.out.println("Rocznik");
+        System.out.println(2004);
+        System.out.println("silnik o pojemności:");
+        System.out.println(2.0);
+    }
+}
